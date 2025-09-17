db1ec33ae335765e1be659c41dc1caf6eb07b21a
Ganc83 <gcandelise@googlemail.com>
2025-09-17 15:59:56 +0200

    MySQL verbindung

--- daten_pdf_xlsx.py
+++ daten_pdf_xlsx.py
@@ -1,8 +1,12 @@
 import pdfplumber
 import pandas as pd
 import re
+import mysql.connector
+import os
+from datetime import datetime
 
-pdf_file = r"C:\Users\........."
+
+pdf_file = r"C:\Users\Davinci\Desktop\test_rechnung.pdf"
 
 rows = []
 
@@ -30,6 +34,42 @@ df = df.iloc[::2, :]
 print(df)
 
 
-df.to_excel("kontoauszug.xlsx", index=False)
+#df.to_excel("kontoauszug.xlsx", index=False)
 
 print("Kontoauszug wurde als kontoauszug.xlsx gespeichert")
+
+daten = df
+
+connection_auszug = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password=os.getenv("MYSQL_PASSWORD"),
+    database="konnto_auszüge"
+)
+cursor_auszüge = connection_auszug.cursor()
+
+
+for index, row in df.iterrows():
+    datum_str = row["Datum"]
+    verwendung = row["Verwendungszweck"]
+    betrag = row["Betrag"]
+    # Umwandeln von DD.MM.YYYY → YYYY-MM-DD
+    datum_mysql = datetime.strptime(datum_str, "%d.%m.%Y").strftime("%Y-%m-%d")
+
+    print(datum_mysql)  # ergibt: 2025-08-29
+
+
+    sql = """
+    INSERT INTO auszug_konnto_01 (Datum, Verwendungszweck, Betrag)
+    VALUES (%s, %s, %s)
+    """
+    values = (datum_mysql, verwendung, betrag)
+    print(values)
+
+    cursor_auszüge.execute(sql, values)
+
+connection_auszug.commit()
+cursor_auszüge.close()
+connection_auszug.close()
+
+print("✅ Daten erfolgreich in MySQL gespeichert!")
